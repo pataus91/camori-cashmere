5aa4e4c2f07c60fb9b0abfe585282da33a8fb312
Silvia Patono <silvia.patono@gmail.com>
2020-05-03 13:22:46 +0200

    Creation of product model controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get "products", to: "products#index"
+  get "products/new", to: "products#new"
+  get "products/:id", to: "products#show", as: :product
 end
